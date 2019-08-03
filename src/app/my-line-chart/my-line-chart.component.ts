8e38ff884811b2925ae5b4567a621fee1610a9ae
Nimrod Abrigo <abrigonimrod@gmail.com>
2019-08-03 23:03:51 +0800

    line chart

--- src/app/my-line-chart/my-line-chart.component.ts
+++ src/app/my-line-chart/my-line-chart.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyLineChartComponent implements OnInit {
 
+  chartOptions = {
+    responsive: true
+  };
+
+  chartData = [
+    { data: [330, 600, 260, 700], label: 'Account A' },
+    { data: [120, 455, 100, 340], label: 'Account B' },
+    { data: [45, 67, 800, 500], label: 'Account C' }
+  ];
+
+  chartLabels = ['January', 'February', 'Mars', 'April'];
+
   constructor() { }
 
   ngOnInit() {
